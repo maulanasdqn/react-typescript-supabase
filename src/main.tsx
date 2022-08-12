0c176fb22574d325d201cdb570e7ccbbbc6498f8
Maulana Sodiqin <maulana@pmg.id>
2022-08-13 01:58:44 +0700

    Maulana Sodiqin | Fix Bug

--- src/main.tsx
+++ src/main.tsx
@@ -7,9 +7,7 @@ import "@unocss/reset/tailwind.css";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-	<React.StrictMode>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
-	</React.StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
